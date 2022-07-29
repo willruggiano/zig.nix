6ca1a7ff6c859817a576ee6c0ab3852d824b765d
Will Ruggiano <wmruggiano@gmail.com>
2022-07-28 18:08:49 -0600

    feat: use ziglang flake

--- flake.nix
+++ flake.nix
@@ -3,11 +3,14 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     pre-commit.url = "github:cachix/pre-commit-hooks.nix";
     utils.url = "github:numtide/flake-utils";
+    zig-flake.url = "github:willruggiano/zig/feat/nix-flake?dir=contrib";
+    zig-flake.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
     self,
     nixpkgs,
+    zig-flake,
     ...
   } @ inputs:
     inputs.utils.lib.eachDefaultSystem (system: let
@@ -29,13 +32,14 @@
 
       devShells.default = pkgs.mkShell {
         name = "zig";
-        nativeBuildInputs = with pkgs; [zig-latest];
+        nativeBuildInputs = with pkgs; [zig-master];
         inherit (self.checks."${system}".pre-commit) shellHook;
       };
-
-      packages.default = pkgs.zig-latest;
     })
     // {
-      overlays.default = import ./packages;
+      overlays = {
+        inherit (zig-flake.overlays) default;
+        release = import ./packages;
+      };
     };
 }
--- packages/default.nix
+++ packages/default.nix
@@ -1,4 +1,3 @@
 final: prev: {
-  zig-latest = final.zig-0-10-0;
   zig-0-10-0 = prev.callPackage ./zig-0-10-0.nix {};
 }
